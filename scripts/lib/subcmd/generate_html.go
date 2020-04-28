5c3711a7895a9f8dad36742518aa0a70a8fe4cb7
tennashi <yuya.gt@gmail.com>
2020-04-29 04:23:19 +0900

    Define the target interface

---
+++ scripts/lib/subcmd/generate_html.go
@@ -0,0 +1,31 @@
+package subcmd
+
+import (
+	"fmt"
+	"path/filepath"
+
+	slacklog "github.com/vim-jp/slacklog/lib"
+)
+
+func GenerateHTML(args []string) error {
+	if len(args) < 4 {
+		fmt.Println("Usage: go run scripts/main.go generate_html {config.json} {templatedir} {indir} {outdir}")
+	}
+	configJSONPath := filepath.Clean(args[0])
+	templateDir := filepath.Clean(args[1])
+	inDir := filepath.Clean(args[2])
+	outDir := filepath.Clean(args[3])
+
+	cfg, err := slacklog.ReadConfig(configJSONPath)
+	if err != nil {
+		return fmt.Errorf("could not read config: %s", err)
+	}
+
+	s, err := slacklog.NewLogStore(inDir, cfg)
+	if err != nil {
+		return err
+	}
+
+	g := slacklog.NewHTMLGenerator(templateDir, s)
+	return g.Generate(outDir)
+}
---
+++ scripts/lib/subcmd/subcmd.go
@@ -0,0 +1,31 @@
+package subcmd
+
+import (
+	"fmt"
+	"os"
+)
+
+func Run() error {
+	fmt.Println(os.Args)
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run scripts/main.go {subcmd}")
+		fmt.Println("  Subcmd:")
+		fmt.Println("    convert_exported_logs")
+		fmt.Println("    download_files")
+		fmt.Println("    download_emoji")
+		fmt.Println("    generate_html")
+		return nil
+	}
+
+	subCmdName := os.Args[1]
+	switch subCmdName {
+	case "generate_html":
+		args := []string{}
+		if len(os.Args) >= 3 {
+			args = os.Args[2:]
+		}
+		return GenerateHTML(args)
+	}
+
+	return fmt.Errorf("Unknown subcmd: %s", subCmdName)
+}
