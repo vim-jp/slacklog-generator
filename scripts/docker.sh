52993f9b139263d0c83085e02214d847adad67a7
kuuote <znmxodq1@gmail.com>
2020-04-29 21:48:34 +0900

    if `make` is installed, using `make` on docker.sh

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -7,10 +7,13 @@ cd "$(dirname "$0")/.." || exit "$?"
 CMD=${1:-serve}
 shift || true
 
-if [[ ! -d slacklog_data ]]; then
-	git archive origin/log-data | tar x
+if which make > /dev/null 2>&1; then
+  make
+else
+  if [[ ! -d slacklog_data ]]; then
+    git archive origin/log-data | tar x
+  fi
+  ./scripts/generate_html.sh
 fi
-
-./scripts/generate_html.sh
 
 docker run --rm -it --volume="$PWD:/srv/jekyll" -p "4000:4000" jekyll/jekyll:pages jekyll ${CMD} "$@"
