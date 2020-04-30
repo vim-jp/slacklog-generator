032fcd4934f85a542fecd029b8bc19b716d4c801
tennashi <yuya.gt@gmail.com>
2020-05-01 01:46:55 +0900

    Check if path is a directory

--- scripts/lib/emoji.go
+++ scripts/lib/emoji.go
@@ -3,6 +3,7 @@ package slacklog
 import (
 	"encoding/json"
 	"io/ioutil"
+	"os"
 )
 
 // EmojiTable : 絵文字データを保持する。
@@ -17,6 +18,13 @@ func NewEmojiTable(path string) (*EmojiTable, error) {
 	emojis := &EmojiTable{
 		urlMap: map[string]string{},
 	}
+
+	if info, err := os.Stat(path); err != nil || info.IsDir() {
+		// pathにディレクトリが存在しても、その場合は無視して、ファイル自体が存在し
+		// なかったこととする。
+		return nil, os.ErrNotExist
+	}
+
 	content, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, err
