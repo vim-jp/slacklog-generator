300731e18e5e99b197bd8a1766adf197f156c0c8
tennashi <yuya.gt@gmail.com>
2020-05-05 22:37:21 +0900

    Fix the field name of EmojiTable

--- scripts/lib/emoji.go
+++ scripts/lib/emoji.go
@@ -5,16 +5,18 @@ import (
 )
 
 // EmojiTable : 絵文字データを保持する。
-// urlMapは絵文字名をキーとし、画像が置いてあるURLが値である。
+// ExtMapは絵文字名をキーとし、画像の拡張子が値である。
+// 絵文字は事前に全てダウンロードしている、という前提であり、そのため拡張子のみ
+// を保持している。
 type EmojiTable struct {
-	URLMap map[string]string
+	ExtMap map[string]string
 }
 
 // NewEmojiTable : pathに指定したJSON形式の絵文字データを読み込み、EmojiTableを
 // 生成する。
 func NewEmojiTable(path string) (*EmojiTable, error) {
 	emojis := &EmojiTable{
-		URLMap: map[string]string{},
+		ExtMap: map[string]string{},
 	}
 
 	if info, err := os.Stat(path); err != nil || info.IsDir() {
@@ -23,7 +25,7 @@ func NewEmojiTable(path string) (*EmojiTable, error) {
 		return nil, os.ErrNotExist
 	}
 
-	if err := ReadFileAsJSON(path, &emojis.URLMap); err != nil {
+	if err := ReadFileAsJSON(path, &emojis.ExtMap); err != nil {
 		return nil, err
 	}
 
