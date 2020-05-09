4fa2548464a931f77afca902eaab0b6175524de0
MURAOKA Taro <koron.kaoriya@gmail.com>
2020-05-09 11:28:51 +0900

    rename templates directory

--- scripts/generate_html.sh
+++ scripts/generate_html.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd "$(dirname "$0")" || exit "$?"
-go run ./main.go generate-html ./config.json ../slacklog_template/ ../slacklog_data/ ../slacklog_pages/
+go run ./main.go generate-html ./config.json ../templates/ ../slacklog_data/ ../slacklog_pages/
