ba22903a544e173206aaf34aa002b22a4ceb0c4c
kuuote <znmxodq1@gmail.com>
2020-05-09 17:13:14 +0900

    build.sh: Hard linking if installed rsync

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,16 @@
 
 set -eu
 
+copy() {
+  if which rsync > /dev/null && [[ -d $1 ]] ; then
+    local dir
+    dir=$(realpath $1)
+    rsync -avP --link-dest=$dir/ $dir/ $2/$1
+  else
+    cp -a $1 $2
+  fi
+}
+
 outdir="_site"
 
 while getopts o: OPT ; do
@@ -12,7 +22,7 @@ done
 
 mkdir -p ${outdir}
 
-cp -a assets ${outdir}
-cp -a emojis ${outdir}
-cp -p favicon.ico ${outdir}
-cp -a files ${outdir}
+copy assets ${outdir}
+copy emojis ${outdir}
+copy favicon.ico ${outdir}
+copy files ${outdir}
