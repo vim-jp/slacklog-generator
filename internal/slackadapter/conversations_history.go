47e0c69a21512b43c251e5d968509b149b137ffe
Kyoichiro Yamada <me@kyoh86.dev>
2020-07-07 00:12:49 +0900

    fix: implement GetConversasionHistories func

--- internal/slackadapter/conversations_history.go
+++ internal/slackadapter/conversations_history.go
@@ -2,9 +2,9 @@ package slackadapter
 
 import (
 	"context"
-	"errors"
 	"time"
 
+	"github.com/slack-go/slack"
 	"github.com/vim-jp/slacklog-generator/internal/slacklog"
 )
 
@@ -27,7 +27,34 @@ type ConversationsHistoryResponse struct {
 }
 
 // ConversationsHistory gets conversation messages in a channel.
-func ConversationsHistory(ctx context.Context, token, channel string, params ConversationsHistoryParams) (*ConversationsHistoryReponse, error) {
-	// TODO: call Slack's conversations.history
-	return nil, errors.New("not implemented yet")
+func ConversationsHistory(ctx context.Context, token, channel string, params ConversationsHistoryParams) (*ConversationsHistoryResponse, error) {
+	client := slack.New(token)
+	res, err := client.GetConversationHistoryContext(ctx, &slack.GetConversationHistoryParameters{
+		ChannelID: channel,
+		Cursor:    string(params.Cursor),
+		Limit:     params.Limit,
+		Oldest:    Timestamp(params.Oldest),
+		Latest:    Timestamp(params.Latest),
+		Inclusive: params.Inclusive,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	var messages []*slacklog.Message
+	for _, m := range res.Messages {
+		messages = append(messages, &slacklog.Message{
+			Message: m,
+		})
+	}
+
+	return &ConversationsHistoryResponse{
+		Ok:       true,
+		Messages: messages,
+		HasMore:  res.HasMore,
+		PinCount: res.PinCount,
+		ResponseMetadata: &NextCursor{
+			NextCursor: Cursor(res.ResponseMetaData.NextCursor),
+		},
+	}, nil
 }
